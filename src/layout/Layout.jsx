8890e54623bf687fe91ce7eeb29da67a3311f7f5
theayusharma <protocolayush@gmail.com>
2025-04-03 08:03:33 +0530

    analytics

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,11 +1,16 @@
 import MenuBar from '../components/MenuBar';
 import FooterDisclaimer from '../components/FooterDisclaimer';
 
+import { Analytics } from '@vercel/analytics/next';
 const Layout = ({ children }) => {
   return (
     <div className="flex flex-col min-h-screen">
       <MenuBar />
-      <main className="flex-grow">{children}</main>
+      <main className="flex-grow">
+				{children}
+				<Analytics/>
+			</main>
+
       <FooterDisclaimer />
     </div>
   );
